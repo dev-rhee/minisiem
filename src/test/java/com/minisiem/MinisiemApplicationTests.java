0c56c6960b6dd9b08a6fa10f65036e26b2f84971
kim hwan <raheekim504@gmail.com>
2026-06-15 21:35:58 +0900

    chore: 프로젝트 초기 세팅 - Spring Boot, Batch, JPA, Swagger, Flyway 의존성 추가

---
+++ src/test/java/com/minisiem/MinisiemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minisiem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinisiemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
